0a9016b49177e9d39f8a76bcbea941dcaa9a8cda
Aben <tntaben@gmail.com>
2015-07-15 13:30:27 +0800

    add hooks

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var through = require('through2')
 
 module.exports = function (b, opts) {
-    b.pipeline.get('deps').push(
-        through.obj(function (row, enc, next) {
-            if (/\.css$/i.test(row.file)) {
-                row.source = ''
-            }
-            this.push(row);
-            next();
-        })
-    )
-    return b
+	b.on('reset', addHooks);
+	addHooks();
+	function addHooks(){
+		b.pipeline.get('deps').push(
+			through.obj(function (row, enc, next) {
+				console.log(row.file)
+				if (/\.css$/i.test(row.file)) {
+					row.source = ''
+				}
+				this.push(row);
+				next();
+			})
+		)
+	}
+	return b
 }
